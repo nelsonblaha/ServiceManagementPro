c13ed245c23dd90dead7c60cbc62d9435c8a622c
rlavery <robertl@uta.edu>
2014-07-13 12:17:26 -0500

    Configured mailers for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'ssrs.reachmail.net',
+  port: 25,
+  user_name: ENV["EMAIL"],
+  password: ENV["PASSWORD"],
+  authentication: "plain",
+  enable_starttls_auto: true 
+  }
+
 end
